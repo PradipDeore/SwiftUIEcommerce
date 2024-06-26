4a7ce67f9b4ee74b9a3f6c7edd3c0baf9470453f
Pradip Vijay Deore <pradip@creativegalileo.com>
2024-06-26 10:54:47 +0530

    new project

---
+++ Ecommerce_iOSApp/Ecommerce_iOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ecommerce_iOSApp
+//
+//  Created by Pradip Deore on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ecommerce_iOSApp/Ecommerce_iOSApp/Ecommerce_iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecommerce_iOSAppApp.swift
+//  Ecommerce_iOSApp
+//
+//  Created by Pradip Deore on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Ecommerce_iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
